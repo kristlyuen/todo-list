b4e0ea1f43adf92097dc8e5617aa9a62eb41fdaa
Kristl Yuen <kristlyuen@users.noreply.github.com>
2023-03-15 21:37:29 -0400

    Updated the functionality and added comments

--- src/components/AddNewItemForm.js
+++ src/components/AddNewItemForm.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-function AddNewItemForm({ listItem, setListItem, list, setList }) {
+// Pass the handleNewItem function in.
+function AddNewItemForm({ handleNewItem }) {
 
+  // Set state for individual list items.
+  const [listItem, setListItem] = React.useState('')
+
+  // Create a function to generate the list when the form is submitted.
   function generateList(event) {  
+    // Prevent the default action of refreshing the page.
     event.preventDefault();
-    
-    const newList = [...list]
-    newList.push(listItem)
-    setList(newList)
-
+    // Run the handleNewItem function on the listItem.
+    handleNewItem(listItem)
+    // Reset the listItem state to blank out the submission field after an item has been entered.
     setListItem('')
   }
   
   return (
     <div className="new-list-item-form">
       <form
+        // Run the generateList function when the form is submitted. 
         onSubmit={generateList}
       >
        
@@ -26,7 +31,9 @@ function AddNewItemForm({ listItem, setListItem, list, setList }) {
           <input
             id="new-list-form-input"
             type="text"
+            // Assign the value to the listItem.
             value={listItem}
+            // When the input is changed, set the listItem to what's entered.
             onChange={(event) => {
           setListItem(event.target.value);
           }}
--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,40 @@ import React from 'react'
 import AddNewItemForm from './AddNewItemForm'
 
 function App() {
-
-  const [listItem, setListItem] = React.useState('')
   
+  // Set state to create the list.
   const [list, setList] = React.useState([])
   
+  // Create a function to run when a new item is added.
+  function handleNewItem(listItem) {
+    // Create a new item and assign it a randomly generated ID.
+    const newItem = {
+      listItem,
+      id: Crypto.randomUUID(),
+    }
+
+    // Spread the existing list and adding the new list item to create a new array.
+    const newItemList = [...list, newItem]
+
+    // Update the list state to the array with the new item.
+    setList(newItemList)
+  }
+
   return (
+    
     <div className="wrapper">
       <div className="list-wrapper">
         <ol className="shopping-list">
-          {list.map((item, index) => (
-            // I know we aren't supposed to use the index, but I was stuck :( 
-            <li key={index}>{item}</li>
+          {/* Map through the list items, use the assigned ID as the key.*/}
+          {list.map((listItem, id) => ( 
+            <li key={id}>{listItem}</li>
           ))}
         </ol>
       </div>
+      
+      {/*    // Render the AddNewItemForm component and pass down the handleNewItem function.*/}
       <AddNewItemForm 
-        listItem={listItem}
-        setListItem={setListItem}
-        list={list}
-        setList={setList}
+        handleNewItem={handleNewItem}
       />
     </div>
   );
